02897725220c39c3a38884c40c92ffdc58c60128
clovzhang <598159298@qq.com>
2019-04-11 11:08:04 +0800

    feat(postcssconfig配置):

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: [require('autoprefixer')]
+  plugins: [require('autoprefixer')({
+    "browsers": [
+      "defaults",
+      "not ie < 11",
+      "last 2 versions",
+      "> 1%",
+      "iOS 7",
+      "last 3 iOS versions"
+    ]
+  })
+  ]
 };
+
+
+
+
